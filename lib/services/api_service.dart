0dc26371e680eb5adaab171dbf8a947c3dbb1f9f
Lak Samphea <samphea.l@bidc.com.kh>
2024-06-21 15:04:20 +0700

    update

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,21 +1,26 @@
 // services/api_service.dart
 
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 import 'package:data_from_internet/models/post.dart';
 
 class ApiService {
   static const String apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+  final Dio _dio = Dio();
 
   Future<List<Post>> getPosts() async {
-    final response = await http.get(Uri.parse(apiUrl));
-    
-    if (response.statusCode == 200) {
-      List<dynamic> body = jsonDecode(response.body);
-      List<Post> posts = body.map((dynamic item) => Post.fromJson(item)).toList();
-      return posts;
-    } else {
-      throw 'Failed to load posts';
+    try {
+      final response = await _dio.get(apiUrl);
+
+      if (response.statusCode == 200) {
+        List<dynamic> body = response.data;
+        List<Post> posts = body.map((dynamic item) => Post.fromJson(item)).toList();
+        return posts;
+      } else {
+        throw 'Failed to load posts';
+      }
+    } catch (error) {
+      throw 'Failed to load posts: $error';
     }
   }
 }
